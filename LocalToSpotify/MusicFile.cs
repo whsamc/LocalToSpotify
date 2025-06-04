da672da179763536b3541d9766b2fafa66445c5c
Sam <whsamchung@gmail.com>
2025-06-03 20:40:20 -0400

    Working on extracting embedded album cover

--- LocalToSpotify/MusicFile.cs
+++ LocalToSpotify/MusicFile.cs
@@ -6,6 +6,7 @@ using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TagLib;
 
 namespace LocalToSpotify
 {
@@ -16,12 +17,14 @@ namespace LocalToSpotify
         public string Artist { get; }
         public string Album { get; }
         public string Filepath { get; }
+        // public Picture? Cover { get; }
 
-        public MusicFile(string title, string artist, string album, string filepath)
+        public MusicFile(string title, string artist, string album,  string filepath)
         {
             this.Title = title;
             this.Artist = artist;
             this.Album = album;
+            // this.Cover = cover;
             this.Filepath = filepath;
         }
     }
