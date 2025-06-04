1633d54e1427c2804905d53d4f850014a76d1ce9
Sam <whsamchung@gmail.com>
2025-06-03 20:26:41 -0400

    Cleaned up MusicFile class and its properties to be public

--- LocalToSpotify/MusicFile.cs
+++ LocalToSpotify/MusicFile.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,10 +12,10 @@ namespace LocalToSpotify
     // This inheritance allows this class to data bind
     public class MusicFile : BindableObject
     {
-        public string Title;
-        public string Artist;
-        public string Album;
-        public string Filepath;
+        public string Title { get; }
+        public string Artist { get; }
+        public string Album { get; }
+        public string Filepath { get; }
 
         public MusicFile(string title, string artist, string album, string filepath)
         {
@@ -21,16 +23,6 @@ namespace LocalToSpotify
             this.Artist = artist;
             this.Album = album;
             this.Filepath = filepath;
-        }
-    }
-
-    public class MusicDirectory
-    {
-        List<MusicFile> _musicDirectory = new List<MusicFile>();
-
-        public MusicDirectory()
-        {
-            this._musicDirectory = new List<MusicFile>();
         }
     }
 }
