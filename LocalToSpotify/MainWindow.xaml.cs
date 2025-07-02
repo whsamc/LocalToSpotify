6b50f51c7a2f36acf3ebf36cf87cbb026b8d4b55
Sam <Sam@SAM-LAPTOPACER>
2025-07-02 12:23:06 -0400

    Moved to main page

--- LocalToSpotify/MainWindow.xaml.cs
+++ LocalToSpotify/MainWindow.xaml.cs
@@ -27,7 +27,6 @@ namespace LocalToSpotify
     {
         public static MainWindow Current;
         public static AppWindow? MyAppWindow;
-        string? FileDirectory;
 
         public MainWindow()
         {
@@ -35,98 +34,6 @@ namespace LocalToSpotify
             Current = this;
             MyAppWindow = this.AppWindow;
             var hwnd = WinRT.Interop.WindowNative.GetWindowHandle(this);
-        }
-
-        // ObservableCollection needs to be a property to properly data bind
-        public ObservableCollection<MusicFile> MusicList { get; set; } = new ObservableCollection<MusicFile>();
-
-        private List<string> MusicFilePathList = new List<string>();
-
-        private HashSet<string> fileExtensions = new HashSet<string>()
-        {
-            ".mp3",
-            ".wav",
-            ".flac",
-            ".opus",
-            ".m4a",
-            ".wma",
-            ".aac"
-        };
-
-        // Get the music file paths for the folder
-        private void ReadThroughFiles(object sender, RoutedEventArgs e)
-        {
-            // Return if FileDirectory is null
-            if (FileDirectory == null) return;
-
-            MusicFilePathList.Clear();
-            MusicList.Clear();
-
-            // trim quotation marks
-            FileDirectory = FileDirectory.Trim('"');
-
-            // If the path is actually just a file
-            if (System.IO.File.Exists(FileDirectory))
-            {
-                // Add single item to the list because of the parse method later
-                MusicFilePathList.Add(FileDirectory);
-            }
-            // If the path is a directory
-            else
-            {
-                // get file paths for all music files inside folder
-                MusicFilePathList = Directory.GetFiles(FileDirectory, "*", SearchOption.AllDirectories).ToList();
-            }
-
-            // Iterate through list and parse metadata from each filepath
-            foreach (var MusicFilePath in MusicFilePathList)
-            {
-                // Check the file type with fileExtensions hashset
-                if (fileExtensions.Contains(Path.GetExtension(MusicFilePath)))
-                {
-                    AddToMusicList(ParseMusicFile(MusicFilePath));
-                }
-            }
-        }
-
-        // Parse the metadata for the music file
-        private MusicFile ParseMusicFile(string filePath)
-        {
-            // trim the string of quotation marks
-            var path = filePath.Trim('"');
-
-            // Try to create a MusicFile object with metadata properties
-            try
-            {
-                // File to open
-                var file = TagLib.File.Create(@path);
-
-                // Create musicfile object
-                return new MusicFile(file.Tag.Title, file.Tag.FirstAlbumArtist, file.Tag.Album, filePath);
-            }
-
-            // Catch wrong format exceptions
-            catch (UnsupportedFormatException e)
-            {
-                return new MusicFile("", "", "", filePath);
-            }
-        }
-
-        private void AddToMusicList(MusicFile song)
-        {
-            MusicList.Add(song);
-        }
-
-        // Switch pages to the Spotify Authentication page
-        private void SpotifyAuthPageButton_Clicked(object sender, RoutedEventArgs e)
-        {
-            
-        }
-
-        // Set the FileDirectory string whenever the entry text box is changed
-        private void ReadFileDirectoryPath(object sender, TextChangedEventArgs e)
-        {
-            FileDirectory = musicFileInput.Text;
         }
     }
 }
