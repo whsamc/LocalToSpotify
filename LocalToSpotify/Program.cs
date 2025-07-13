68e38a1d4cf24d99c807f1f1a7933b7b52b241dd
Sam <whsamchung@gmail.com>
2025-07-13 18:56:28 -0400

    Redirection upon launching instance

--- LocalToSpotify/Program.cs
+++ LocalToSpotify/Program.cs
@@ -1,11 +1,14 @@
-﻿using System;
+﻿using Microsoft.UI.Dispatching;
+using Microsoft.Windows.AppLifecycle;
+using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using System.Runtime.InteropServices;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.UI.Dispatching;
-using Microsoft.UI.Xaml;
-using Microsoft.Windows.AppLifecycle;
+using Windows.ApplicationModel.Activation;
 
 namespace LocalToSpotify
 {
@@ -16,91 +19,66 @@ namespace LocalToSpotify
 
     public class Program
     {
+        private static DispatcherQueue dispatcherQueue;
+
         [STAThread]
-        static int Main(string[] args)
+        static void Main(string[] args)
         {
             WinRT.ComWrappersSupport.InitializeComWrappers();
-            bool isRedirect = DecideRedirection();
+            AppInstance.GetCurrent().Activated += OnActivated;
+
+            bool isRedirect = DecideRedirection().GetAwaiter().GetResult();
 
             if (!isRedirect)
             {
-                Application.Start((p) =>
+                Microsoft.UI.Xaml.Application.Start((p) =>
                 {
-                    var context = new DispatcherQueueSynchronizationContext(
-                        DispatcherQueue.GetForCurrentThread());
+                    dispatcherQueue = DispatcherQueue.GetForCurrentThread();
+                    var context = new DispatcherQueueSynchronizationContext(dispatcherQueue);
                     SynchronizationContext.SetSynchronizationContext(context);
-                    _ = new App();
+                    new App();
                 });
             }
-
-            return 0;
         }
 
-        private static bool DecideRedirection()
+        private static async Task<bool> DecideRedirection()
         {
-            bool isRedirect = false;
-            AppActivationArguments args = AppInstance.GetCurrent().GetActivatedEventArgs();
-            ExtendedActivationKind kind = args.Kind;
-            AppInstance keyInstance = AppInstance.FindOrRegisterForKey("MySingleInstanceApp");
+            var mainInstance = Microsoft.Windows.AppLifecycle.AppInstance.FindOrRegisterForKey("main");
+            var activatedEventArgs = Microsoft.Windows.AppLifecycle.AppInstance.GetCurrent().GetActivatedEventArgs();
 
-            if (keyInstance.IsCurrent)
+            if (!mainInstance.IsCurrent)
             {
-                keyInstance.Activated += OnActivated;
-            }
-            else
-            {
-                isRedirect = true;
-                RedirectActivationTo(args, keyInstance);
+                // Redirect the activation (and args) to the "main" instance, and exit.
+                await mainInstance.RedirectActivationToAsync(activatedEventArgs);
+                return true;
             }
 
-            return isRedirect;
+            return false;
         }
 
-        [DllImport("kernel32.dll", CharSet = CharSet.Unicode)]
-        private static extern IntPtr CreateEvent(
-    IntPtr lpEventAttributes, bool bManualReset,
-    bool bInitialState, string lpName);
+        // Handle Redirection
+        public static void OnActivated(object sender, AppActivationArguments args)
+        {
+            if (args.Kind == ExtendedActivationKind.Protocol)
+            {
+                var protocolArgs = (ProtocolActivatedEventArgs)args.Data;
+                dispatcherQueue.TryEnqueue(() =>
+                {
+                    if (protocolArgs.Uri.Authority == "oauthcallback")
+                    {
+                        Debug.WriteLine(protocolArgs.Uri);
+                        // App.AppWindow.OnUriCallback(protocolArgs.Uri);
+                    }
+                    SetForegroundWindow(App.WindowHandle);
+                });
 
-        [DllImport("kernel32.dll")]
-        private static extern bool SetEvent(IntPtr hEvent);
+            }
+        }
 
-        [DllImport("ole32.dll")]
-        private static extern uint CoWaitForMultipleObjects(
-            uint dwFlags, uint dwMilliseconds, ulong nHandles,
-            IntPtr[] pHandles, out uint dwIndex);
-
+        // P/Invoke declaration for SetForegroundWindow
         [DllImport("user32.dll")]
-        static extern bool SetForegroundWindow(IntPtr hWnd);
+        private static extern bool SetForegroundWindow(IntPtr hWnd);
 
-        private static IntPtr redirectEventHandle = IntPtr.Zero;
-
-        // Do the redirection on another thread, and use a non-blocking
-        // wait method to wait for the redirection to complete.
-        public static void RedirectActivationTo(AppActivationArguments args,
-                                                AppInstance keyInstance)
-        {
-            redirectEventHandle = CreateEvent(IntPtr.Zero, true, false, null);
-            Task.Run(() =>
-            {
-                keyInstance.RedirectActivationToAsync(args).AsTask().Wait();
-                SetEvent(redirectEventHandle);
-            });
-
-            uint CWMO_DEFAULT = 0;
-            uint INFINITE = 0xFFFFFFFF;
-            _ = CoWaitForMultipleObjects(
-               CWMO_DEFAULT, INFINITE, 1,
-               [redirectEventHandle], out uint handleIndex);
-
-            // Bring the window to the foreground
-            Process process = Process.GetProcessById((int)keyInstance.ProcessId);
-            SetForegroundWindow(process.MainWindowHandle);
-        }
-
-        private static void OnActivated(object sender, AppActivationArguments args)
-        {
-            ExtendedActivationKind kind = args.Kind;
-        }
     }
 
 }
