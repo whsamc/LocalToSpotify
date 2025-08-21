650f8ec118c82ea390f960f7ed33d3f00cec9fe5
Sam <whsamchung@gmail.com>
2025-08-20 22:45:04 -0400

    singleton implementation

--- LocalToSpotify/Data.cs
+++ LocalToSpotify/Data.cs
@@ -1,15 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WinRT.LocalToSpotifyVtableClasses;
 
 namespace LocalToSpotify
 {
-    internal class Data
+    internal sealed class Data
     {
+        /*
+         *  Singleton implementation for Data class.
+         *  Uses .NET 6+ Lazy<T> to ensure thread-safe, lazy initialization.
+         */
+        private static readonly Lazy<Data> data = new Lazy<Data>(() => new Data());
+        public static Data Instance { get { return data.Value; } }
+        private Data() { }
+
+        
         private string _spotifyToken = "";
         public string SpotifyToken { get { return _spotifyToken; } set { _spotifyToken = value; } }
         public Profile userProfile = new Profile();
+
+
+    }
     }
 }
