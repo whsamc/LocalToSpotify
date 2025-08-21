7032d86fb63ec090a7306acb33b8f72f657d826f
Sam <whsamchung@gmail.com>
2025-08-20 22:29:02 -0400

    class to centralize information to retrieve from

---
+++ LocalToSpotify/Data.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalToSpotify
+{
+    internal class Data
+    {
+        private string _spotifyToken = "";
+        public string SpotifyToken { get { return _spotifyToken; } set { _spotifyToken = value; } }
+        public Profile userProfile = new Profile();
+    }
+}
