eafae3cd27029944c43c772b06cab8f84ae144ca
Sam <Sam@SAM-LAPTOPACER>
2025-09-22 17:07:55 -0400

    debug lines and adjusting exception catches

--- LocalToSpotify/Search.cs
+++ LocalToSpotify/Search.cs
@@ -20,6 +20,7 @@ namespace LocalToSpotify
         {
             try
             {
+                Debug.WriteLine("Building search query...");
                 // Search query through spotify API
                 StringBuilder sb = new StringBuilder("https://api.spotify.com/v1/search?q=");
                 sb.Append(info.Title);
@@ -29,28 +30,43 @@ namespace LocalToSpotify
                 sb.Append(info.Album);
                 sb.Append("&type=track&limit=3");
 
+                Debug.WriteLine("Adjusting search query parameters...");
                 // Set the authorization header with your access token
                 client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", spotifyToken);
 
-                // Make the GET request to the Spotify API
-                var response = await client.GetAsync(sb.ToString());
-                if (response.IsSuccessStatusCode)
+                Debug.WriteLine("Sending search request to Spotify API...");
+                try
                 {
-                    // Parse the JSON response into a json object
-                    var jsonResponse = await response.Content.ReadFromJsonAsync<JsonObject>();
-                    SpotifySearchResponse spotifySearch = JsonConvert.DeserializeObject<SpotifySearchResponse>(response.ToString());
+                    // Make the GET request to the Spotify API
+                    var response = await client.GetAsync(sb.ToString());
 
-                    // Debug output of search results
-                    foreach (var item in spotifySearch.tracks.items)
+                    if (response.IsSuccessStatusCode)
                     {
-                        Debug.WriteLine(item.name + " by " + item.artists);
-                    }
+                        Debug.WriteLine("Search request successful. Parsing JSON response into object...");
+                        // Parse the JSON response into a json object
+                        var jsonResponse = await response.Content.ReadFromJsonAsync<JsonObject>();
 
-                    return spotifySearch;
+                        Debug.WriteLine("Deserializing JSON response into SpotifySearchResponse object...");
+                        SpotifySearchResponse spotifySearch = JsonConvert.DeserializeObject<SpotifySearchResponse>(response.ToString());
+
+                        Debug.WriteLine($"{info.Title} by {info.Artist} Results:");
+                        // Debug output of search results
+                        foreach (var item in spotifySearch.tracks.items)
+                        {
+                            Debug.WriteLine(item.name + " by " + item.artists);
+                        }
+
+                        return spotifySearch;
+                    }
+                    else
+                    {
+                        throw new Exception($"Error fetching data: {response.ReasonPhrase}");
+                    }
                 }
-                else
+                catch(Exception ex)
                 {
-                    throw new Exception($"Error fetching data: {response.ReasonPhrase}");
+                    Debug.WriteLine("Exception during GET request: " + ex.Message);
+                    return null;
                 }
             }
             catch (Exception ex)
