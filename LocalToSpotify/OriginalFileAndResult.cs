8f6df295b71f573587c83cb0014fd3287de4fdb8
Sam <Sam@SAM-LAPTOPACER>
2025-10-07 15:13:05 -0400

    To pass data to UI gridview as one object

---
+++ LocalToSpotify/OriginalFileAndResult.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalToSpotify
+{
+    class OriginalFileAndResult
+    {
+        public MusicInfo originalFile { get; set; }
+        public SpotifySearchResponse result {get; set;}
+
+        public OriginalFileAndResult(MusicInfo originalFile, SpotifySearchResponse result)
+        {
+            this.originalFile = originalFile;
+            this.result = result;
+        }
+    }
+}
